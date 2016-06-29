25a8723d5d48d37bba750bacfa9e796b9dd86d30
William <wceriale@cs.washington.edu>
2016-06-29 11:22:40 -0700

    added option to keep number of cards, redraw option has been made

--- src/VideoPokerClient.java
+++ src/VideoPokerClient.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 
 public class VideoPokerClient {
 	public static final int HAND_SIZE = 5;
@@ -16,9 +18,26 @@ public class VideoPokerClient {
 
 		// Print hand out
 		printHand(hand);
-
+		System.out.println(deck.toString());
+		System.out.println("Enter numbers seperated by spaces to indicate which cards to keep");
 		String userLine = input.nextLine();
+		processInput(userLine, hand, deck);
+		printHand(hand);
+		System.out.println(deck.toString());
 
+	}
+
+	public static void processInput(String input, Card[] hand, Deck deck) {
+		Scanner s = new Scanner(input);
+		Set<Integer> set = new HashSet<Integer>();
+		for(int i = 1; i <= HAND_SIZE; i++)
+			set.add(i);
+		while(s.hasNext()) {
+			set.remove(s.nextInt());
+		}
+		for(Integer i : set) {
+			hand[i - 1] = deck.draw();
+		}
 	}
 
 	public static void deal(Card[] hand, Deck deck) {
@@ -28,9 +47,12 @@ public class VideoPokerClient {
 	}
 
 	public static void printHand(Card[] hand) {
+		System.out.println();
+		System.out.println();
 		for(int i = 0; i < hand.length; i++) {
 			System.out.print((i + 1) + ") " + hand[i] + "   ");
 		}
+		System.out.println();
 		System.out.println();
 	}
 
